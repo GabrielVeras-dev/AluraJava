765b07c2a4998ccf3bd749b38d88dcdd850ac8b5
Gabriel Veras <61162241+GabrielVeras-dev@users.noreply.github.com>
2024-01-08 10:22:06 -0300

    Adicionado arquivos via upload do github
    Adicionado arquivos do projeto screenmatch.

---
+++ screenmatch/src/Filme.java
@@ -0,0 +1,28 @@
+public class Filme {
+    String nome;
+    int anoLancamento;
+    boolean incluidoNoPlano;
+   private double somaDasAvaliacoes;
+   private int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+        System.out.println("Incluído no plano: " + incluidoNoPlano);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
